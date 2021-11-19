e9f3635bdda2750df21beb19ecc6c02042d1a463
rtakak <rtakak@gmail.com>
2021-11-19 16:33:03 +0300

    First release

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# This is a sample Python script.
-
 import math
 import random
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import cv2
 import numpy as np
 
